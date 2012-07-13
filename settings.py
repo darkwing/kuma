8f000ecfa3453f31f6305d3b312a5d45be1b8d70
David Walsh <dwalsh@mozilla.com>
2012-07-13 09:58:47 -0500

    Adding ability to upload attachments.

--- settings.py
+++ settings.py
@@ -549,6 +549,7 @@ MINIFY_BUNDLES = {
         ),
         'wiki': (
             'css/wiki.css',
+            'css/modals.css',
             'css/wiki-screen.css',
             'syntaxhighlighter/styles/shCore.css',
             'syntaxhighlighter/styles/shThemeDefault.css',
@@ -664,6 +665,7 @@ MINIFY_BUNDLES = {
             'syntaxhighlighter/scripts/shBrushPhp.js',
             'syntaxhighlighter/scripts/shBrushXml.js',
             'syntaxhighlighter/scripts/shBrushPlain.js',
+            'js/mdn/jquery.simplemodal.1.4.1.min.js',
             'js/wiki.js',
             'js/main.js',
             'js/libs/jqueryui.min.js',
